0c682a3e2318a1451858f5eda07947bddbe63957
Anik Mosfeq <mosfeqanik01@gmail.com>
2021-11-17 14:32:38 +0600

    first commit

---
+++ lib/models/order.dart
@@ -0,0 +1,17 @@
+
+import 'package:food_factory/models/food.dart';
+import 'package:food_factory/models/restaurant.dart';
+
+class Order {
+  final Restaurant restaurant;
+  final Food food;
+  final String date;
+  final int quantity;
+
+  Order({
+    this.date,
+    this.restaurant,
+    this.food,
+    this.quantity,
+  });
+}
